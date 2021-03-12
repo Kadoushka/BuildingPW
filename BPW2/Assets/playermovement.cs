e97a54539d16c2b4b304568240336959332a7dd6
Tom Zevering <tom.zevering@student.hku.nl>
2021-03-12 11:56:45 +0100

    no message

--- BPW2/Assets/playermovement.cs
+++ BPW2/Assets/playermovement.cs
@@ -6,12 +6,20 @@ public class playermovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
-    public float gravity = -9.81f;
+    public float gravity = 0;
+    public float gravityOffTime = 4;
+    public float cooldownTime = 8;
     Vector3 velocity;
+    public bool gravityOff;
+    public bool cooldown;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gravityOff = false;
+        cooldown = false;
+        GravityController.SharedInstance.gravity = 2;
     }
 
     // Update is called once per frame
@@ -20,12 +28,74 @@ public class playermovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown == false)
+        {
+            StopCoroutine(Cooldown());
+            StartCoroutine(Cooldown());
+            Debug.Log("Space key was pressed.");
+            if (gravityOff == false)
+            {
+                StartCoroutine(ChangeGravity());
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && gravityOff == true)
+        {
+            StopCoroutine(Cooldown());
+            StartCoroutine(Cooldown());
+
+            Debug.Log("Space key was released.");
+            
+            
+                StopCoroutine(ChangeGravity());
+                GravityOff();
+                gravityOff = false;
+            
+        }
+
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+
+    }
+
+    private void GravityOn()
+    {
+        GravityController.SharedInstance.gravity = 1;
+        gravity = -10;
+    }
+    private void GravityOff()
+    {
+        GravityController.SharedInstance.gravity = 3;
+        gravity = 0;
+        velocity.y = 0;
+    }
+
+    private IEnumerator ChangeGravity()
+    {
+        gravityOff = true;
+        GravityOff();
+        yield return new WaitForSeconds(gravityOffTime);
+
+        GravityOn();
+        gravityOff = false;
+
+
+    }
+    private IEnumerator Cooldown()
+    {
+        GravityController.SharedInstance.gravity = 1;
+        cooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        cooldown = false;
+        GravityController.SharedInstance.gravity = 2;
+
     }
 }
- 
+//1 is herladen
+//2 is vol
+//3 is actief
